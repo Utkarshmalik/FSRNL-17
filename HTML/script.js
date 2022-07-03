5e54e8ca2dfd12b0cea833cece1e517f66c8ab38
utmalik <utmalik@amazon.com>
2022-07-03 22:02:38 +0530

    Employee API fetch and search

--- HTML/script.js
+++ HTML/script.js
@@ -1,64 +1,94 @@
 
-const button = document.getElementById("addAlbumButton");
-const albums= document.getElementsByClassName("albums")[0];
+const allUsers=document.getElementsByClassName('users')[0];
+const message=document.getElementsByClassName('message')[0];
+const searchField=document.getElementsByName("user-search")[0];
 
-button.addEventListener("click",function(){
-
-    const image = document.getElementById("imageURL").value;
-    const description = document.getElementById("description").value;
-    const time = document.getElementById("time").value;
+var allUsersData;
 
 
-    const album=createNewAlbum(image,description,time);
+searchField.addEventListener('keyup', function(){
 
-    albums.appendChild(album);
+    const value=this.value;
+
+    const filteredUsers=allUsersData.filter((user)=>{
+        return user.firstName.toLowerCase().startsWith(value);
+    })
+
+    removeExistingUsers();
+    loadUsers(filteredUsers);
+    showUsers();
+});
+
+function removeExistingUsers(){
+    allUsers.innerHTML='';
+}
+
+fetch("https://dummyapi.io/data/v1/user",{
+    "method":"GET",
+    headers:{
+        "app-id":"62c1b1b65b25e6a595ee427b"
+    }})
+    .then(response=>response.json())
+    .then(data=>{
+        allUsersData=data.data;
+        loadUsers(data.data);
+        hideLoadingMessage();
+        showUsers();
+    });
+
+    function hideLoadingMessage(){
+        message.style.display="none";
+    }
+
+    function showUsers(){
+        allUsers.style.display="flex";
+    }
+
+
+    function loadUsers(users){
+
+
+        users.forEach((user)=>{
+
+            //try to make a card 
+
+            const userCard=createUserCard(user);
+            allUsers.appendChild(userCard);
+
+        })
+    }
+
+    function createUserCard(user){
+
+        const userDiv=document.createElement('div');
+        userDiv.className="user";
+        userDiv.id=user.id;
+
+        const userImageDiv=document.createElement('div');
+        userImageDiv.className="user-image";
+
+        const imageElement=document.createElement('img');
+        imageElement.setAttribute("src",user.picture);
+
+        userImageDiv.appendChild(imageElement);
+
+        const userDetailsDiv=document.createElement('div');
+        const heading=document.createElement('h2');
+        heading.textContent=` ${user.title} ${user.firstName} ${user.lastName}`;
+
+        userDetailsDiv.appendChild(heading);
+
+        const button=document.createElement('button');
+        button.textContent="See Complete Details";
+
+        userDiv.appendChild(userImageDiv);
+        userDiv.appendChild(userDetailsDiv);
+        userDiv.appendChild(button);
+
+        return userDiv;
+    
+    }
+
+
 
     
-})
-
-function createNewAlbum(imageURL,description,time){
-
-    const albumDiv=document.createElement('div');
-    albumDiv.className="album";
-
-    const thumbNailDiv=document.createElement('div');
-    thumbNailDiv.className="thumbnail";
-    const image= document.createElement('img');
-    image.setAttribute('src',imageURL);
-    thumbNailDiv.appendChild(image);
-
-
-    const details=document.createElement('div');
-    details.className="details";
-
-
-    const para=document.createElement('p');
-    para.textContent=description;
-
-    const detailsFooter=document.createElement('div');
-    detailsFooter.className="details-footer";
-
-    const buttonsDiv=document.createElement('div');
-    const button1=document.createElement('button');
-    button1.textContent="View";
-    const button2=document.createElement('button');
-    button2.textContent="Edit";
-
-    buttonsDiv.appendChild(button1);
-    buttonsDiv.appendChild(button2);
-
-    const timeElement=document.createElement('span');
-    timeElement.textContent=time+ " min";
-
-    detailsFooter.appendChild(buttonsDiv);
-    detailsFooter.appendChild(timeElement);
-
-    details.appendChild(para);
-    details.appendChild(detailsFooter);
-
-
-    albumDiv.appendChild(thumbNailDiv);
-    albumDiv.appendChild(details);
-
-    return albumDiv;
-}
