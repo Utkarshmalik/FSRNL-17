4647280d5a5468b2558bda6641e78420ab654983
utmalik <utmalik@amazon.com>
2022-08-07 22:40:08 +0530

    basic CRUD operations in Nodejs

--- Nodejs/basicApp/server.js
+++ Nodejs/basicApp/server.js
@@ -1,33 +1,99 @@
 var express= require("express");
 var bodyParser = require('body-parser')
+const random = require('random');
+const res = require("express/lib/response");
+
 var app=express();
 
 app.use(bodyParser.json());
 
+//CRUD
 
-app.get('/',(req,res)=>{
-    console.log(req.body);
-    res.send("Hello");
+var users=[
+    {id:1341235,name:"Utkarsh",age:27},
+    {id:293745134,name:"Rahul",age:33},
+    {id:23525,name:"Pawan",age:35},
+    {id:2351541,name:"Shreya",age:22}
+];
+
+
+app.get('/api/users',(req,res)=>{
+    res.json(users);
 })
 
-app.post('/login',(req,res)=>{
 
-    const userName=req.body.userName;
-    const password=req.body.password;
+//user should be able to get a user by its id
+app.get('/api/users/:id',(req,res)=>{
 
-    //validate this 
+    const id= parseInt(req.params.id);
+    
+    const user=users.find((user)=>user.id===id);
 
-    console.log(userName);
+    if(!user){
+        res.status(404).json({message:"User doesnot exists"});
+    }
 
-
-    res.send("fee");
+    res.send(user);
 })
 
-app.get('/users',(req,res)=>{
-    res.send("Users");
+
+//create a new user 
+app.post('/api/users',(req,res)=>{
+
+    if(!req.body.name || !req.body.age){
+        res.status(400).json({message:"Invalid data!"});
+    }
+    
+    const user={name:req.body.name,age:req.body.age,id:random.int(1,10000000)};
+    users.push(user);
+    res.json(user);
+})
+
+
+//update the user via idd
+app.put('/api/users/:id',(req,res)=>{
+
+    const id=req.params.id;
+    const user=users.find((user)=>user.id===parseInt(id));
+
+    if(!user){
+        res.status(404).send({message:"Invalid user id"});
+    }
+
+    const keys=Object.keys(req.body);
+    console.log(keys);
+
+    keys.forEach((key)=>{
+        if(!user[key]){
+            res.status(400).send({message:"Invalid details passed in the body"});
+        }
+        user[key]=req.body[key];
+    })
+
+    res.send(user);
+})
+
+
+
+//delete on the basis of id 
+
+app.delete('/api/users/:id',(req, res)=>{
+
+    const id=req.params.id;
+    const user=users.find((user)=>user.id===parseInt(id));
+
+    if(!user){
+        res.status(404).send({message:"Invalid user id"});
+    }
+
+    users=users.filter((user)=>user.id!==parseInt(id));
+    res.send(user);
 })
 
 
 app.listen(8001,()=>{
     console.log("your server is running on port 8000");
 })
+
+
+
